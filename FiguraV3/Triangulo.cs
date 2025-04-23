add4eba585689f22550ce2d2b13d47ea257f7f6a
David Ccolcca <draigner143@gmail.com>
2025-04-23 18:06:40 -0500

    Triangulo

---
+++ FiguraV3/Triangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguraV3
+{
+    internal class Triangulo : Figura
+    {
+        private float bT;
+        private float hT;
+
+        public Triangulo(float bT, float hT)
+        {
+            this.bT = bT;
+            this.hT = hT;
+        }
+
+        public override float CalculateArea()
+        {
+            return (bT * hT) / 2;
+        }
+    }
+}
