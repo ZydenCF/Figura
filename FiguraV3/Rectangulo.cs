70d6b01e609fd3003bfac665f4d677fe9ab13394
David Ccolcca <draigner143@gmail.com>
2025-04-23 18:09:51 -0500

    Rectangulo

---
+++ FiguraV3/Rectangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguraV3
+{
+    public class Rectangulo : Figura
+    {
+        private float b;
+        private float h;
+
+        public Rectangulo(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public override float CalculateArea()
+        {
+            return b * h;
+        }
+    }
+}
