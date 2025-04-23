705c08b134fccd84431f190761e84f4e96411b61
David Ccolcca <draigner143@gmail.com>
2025-04-23 18:12:06 -0500

    Circulo

---
+++ FiguraV3/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguraV3
+{
+    public class Circulo : Figura
+    {
+        private float r;
+
+        public Circulo(float r)
+        {
+            this.r = r;
+        }
+
+        public override float CalculateArea()
+        {
+            return r * r * 3.14f;
+        }
+    }
+}
